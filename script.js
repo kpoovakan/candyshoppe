ac8f15a9dcf024ec51549ae9df43249b69df3fa4
Pravina Ivy <217997837+kpoovakan@users.noreply.github.com>
2026-02-11 14:27:05 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ function loadButtons() {
 }
 
 function play() {
-	document.getElementById("output").innerHTML = `<iframe width="700px" height="395" src="https://www.youtube.com/embed/xvFZjo5PgG0?autoplay=1&modestbranding=1&controls=0&rel=0"
-	title="muahahahahaha" frameborder="0" allow="autoplay; encrypted-media;" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+	document.getElementById("output").innerHTML = `<div class="main-picture"><iframe width="700px" height="395" src="https://www.youtube.com/embed/xvFZjo5PgG0?autoplay=1&modestbranding=1&controls=0&rel=0&loop=1"
+	title="muahahahahaha" frameborder="0" allow="autoplay; encrypted-media;" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>`
 
 }
+
 
